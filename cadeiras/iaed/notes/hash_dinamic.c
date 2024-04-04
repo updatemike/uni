62a022dde7217df08a320fe91412050c53b66a1f
Michael Maycock <michael.maycock@tecnico.ulisboa.pt>
2024-04-04 22:39:01 +0100

    Add hash table dinamic resizing

---
+++ cadeiras/iaed/notes/hash_dinamic.c
@@ -0,0 +1,175 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//The main diference here is that I count the number of added elements with N
+//I then use this N in insertHash to call expand() if N > M / 2
+
+typedef struct node {
+  int n;
+} Node;
+
+static int M;
+static Node **table;
+static int N;
+
+static void getCommand();
+static int getSize();
+int getNumber();
+void insertHash(Node *node);
+
+Node **init(int size) {
+  M = size;
+  Node **t = malloc(size*sizeof(Node *));
+  for(int i = 0; i < size; i++) {
+    t[i] = NULL;
+  }
+  return t;
+}
+
+int hash(int i) {
+  return i % M;
+}
+
+Node *createNode(int n) {
+  Node *new = malloc(sizeof(Node));
+  new->n = n;
+  return new;
+}
+
+void searchHash() {
+  int n = getNumber();
+  int h = hash(n);
+  for(int i = 0; i < M; i++) {
+    if(table[h]->n == n) {
+      printf("Number %d is in the table.\n", n);
+      return;
+    }
+  }
+  printf("Number %d is not in the table.\n", n);
+}
+
+void expand() {
+  Node **cpy = table;
+  table = init(M + M);
+  N = 0;
+  for(int i = 0; i < M / 2; i++) {
+    if(cpy[i] != NULL)
+      insertHash(cpy[i]);
+  }
+}
+
+void insertHash(Node *node) {
+  int h = hash(node->n);
+  while(table[h] != NULL)
+    if(h == M - 1) h = 0; //If last index, goes back to index 0
+    else h++;
+  table[h] = node;
+  N++;
+  if(N > M / 2) expand();
+}
+
+/*After removing a node you have to re-add all adjacent nodes until you find
+and empty index.*/
+void reHash(int n) {
+  for(int i = (n + 1) % M; table[i] != NULL; i = (i + 1) % M) {
+    Node *node = table[i];
+    table[i] = NULL;
+    insertHash(node);
+  }
+}
+
+void removeHash() {
+  int n = getNumber();
+  int h = hash(n);
+  while(table[h] != NULL) {
+    if(table[h]->n == n) break;
+    else h = (h + 1) % M;
+  }
+  if(table[h] == NULL) {
+    printf("Number %d is not in the table.\n", n);
+    return;
+  }
+  //Free the node and make room on the table 
+  free(table[h]); 
+  table[h] = NULL;
+  //re-hash the rest of the table until you find an empty index
+  reHash(h);
+}
+
+void countHash() {
+  int count = 0;
+  for(int i = 0; i < M; i++) {
+    if(table[i]) count++;
+  }
+  printf("There are %d elements in the table.\n", count);
+}
+
+void printHash() {
+  for(int i = 0; i < M; i++) {
+    if(table[i]) printf("| %d |", table[i]->n);
+    else printf("| NULL |");
+  }
+  printf("\n");
+}
+
+int main() {
+  N = 0;
+  int size = getSize();
+  table = init(size);
+  getCommand();
+  printf("Exiting.\n");
+  return 0;
+}
+
+static void getCommand() {
+  int command = -1;
+  int n;
+  Node *node;
+  while(command != 0) {
+    printf("1: Insert, 2: Remove, 3: Count, 4: Search, 5: Print, 0: quit.\n");
+    printf("Input: ");
+    scanf("%d", &command);
+    switch(command) {
+      case 1:
+        n = getNumber();
+        node = createNode(n);
+        insertHash(node);
+        break;
+      case 2:
+        removeHash();
+        break;
+      case 3:
+        countHash();
+        break;
+      case 4:
+        searchHash();
+        break;
+      case 5:
+        printHash();
+        break;
+      case 0:
+        break;
+      default:
+        printf("Invalid number.\n");
+        break;
+    }
+  }
+}
+
+static int getSize() {
+  int size;
+  printf("Size of the hash table: ");
+  while(scanf("%d", &size) != 1 && size < 1)
+    printf("Plese insert a valid number: ");
+  return size;
+}
+
+int getNumber() {
+  int i;
+  printf("Type number: ");
+  while(scanf("%d",&i) != 1) {
+    
+    printf("Type number: ");
+  }
+  return i;
+}
